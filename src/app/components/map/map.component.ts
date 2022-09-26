ece4d21dc1d4785f70774e010cb2cba4f93a68c8
Seraj Haqiqi <haqiqi.seraj@gmail.com>
2022-09-27 00:56:07 +0330

    Fix: Make location marks reactive

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,6 +1,7 @@
-import { AfterViewInit, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AfterViewInit, Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import * as L from "leaflet";
-import type {Location} from "../../app.component"
+import type {Location} from "../../app.component";
+
 L.Marker.prototype.options.icon = L.icon({
   iconUrl: "/assets/marker-icon.png",
   shadowUrl: "/assets/marker-shadow.png"
@@ -11,38 +12,45 @@ L.Marker.prototype.options.icon = L.icon({
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent implements OnInit, AfterViewInit {
+export class MapComponent implements OnInit, AfterViewInit, OnChanges {
   private map!: L.Map;
   @Input() defaultLocation!: L.LatLngExpression;
   @Input() defaultZoomLevel: number = 9;
-  @Input() savedLocations: Location[] = [];
-
+  @Input() savedLocations!: Location[];
   @Output() onMapClick: EventEmitter<any> = new EventEmitter();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.dropMarkers()
   }
 
   ngAfterViewInit(): void {
     this.map = L.map('map').setView(this.defaultLocation, this.defaultZoomLevel);
+    
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
     }).addTo(this.map);
 
-    this.savedLocations.forEach((location:Location)=>{
-      L.marker(location.latlng).addTo(this.map)
-      .bindPopup(`<b>${location.name}</b><br>
-        <b>type: </b> ${location.type}<br>
-        <b>latlng: </b> ${location.latlng}<br>
-        <b>logo: </b> ${location.logo}<br>
-      `)
-    })
+    this.dropMarkers();
  
     this.map.on('click',(e)=>{
       this.onMapClick.emit(e)
     })
     
+  }
+
+  dropMarkers(): void{
+    this.savedLocations.forEach((location)=>{
+      L.marker((location as Location).latlng).addTo(this.map)
+      .bindPopup(`<b>${(location as Location).name}</b><br>
+        <b>type: </b> ${(location as Location).type}<br>
+        <b>latlng: </b> ${(location as Location).latlng}<br>
+        <b>logo: </b> ${(location as Location).logo}<br>
+      `)
+    })
   }
 
 }
