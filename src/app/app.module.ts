01e9eb519f1cca4189235f24d3a20853d3adfe86
Seraj Haqiqi <haqiqi.seraj@gmail.com>
2022-09-26 21:21:59 +0330

    feat: Add notification animation and style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapComponent } from './components/map/map.component';
 import { PopupComponent } from './components/popup/popup.component';
-
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MiniMapComponent } from './components/mini-map/mini-map.component';
 @NgModule({
   declarations: [
     AppComponent,
     MapComponent,
-    PopupComponent
+    PopupComponent,
+    MiniMapComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    ToastrModule.forRoot(),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
