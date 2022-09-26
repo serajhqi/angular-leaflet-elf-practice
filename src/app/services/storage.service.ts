cb0f9eb333b0470ba70df1dfdce53ec507a59708
Seraj Haqiqi <haqiqi.seraj@gmail.com>
2022-09-26 19:34:45 +0330

    feat: Add location methods and save implementation

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
-
+import { nanoid } from 'nanoid';
+import { Location } from '../app.component';
+const MAINKEY = "locations"
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class StorageService {
 
-  constructor() { }
+	constructor() { }
+
+	saveLocation(location: Location) {
+		const locations = this.getLocations();
+		location.id = nanoid(5);
+		locations.push(location);
+		localStorage.setItem(MAINKEY, JSON.stringify(locations));
+	}
+
+	getLocation(id: string): Location | undefined {
+		const locations = this.getLocations();
+		return locations.filter(location => location.id === id)?.[0]
+	}
+
+	getLocations(): Location[] {
+		return localStorage.getItem(MAINKEY) ?
+			JSON.parse(localStorage.getItem('locations') as string):[];
+	}
+
+	updateLocation(id: string) {
+	}
+
+	removeLocation(key: string): Location[] {
+		localStorage.removeItem(key);
+		return [];
+	}
+
+	clear() {
+		localStorage.clear();
+	}
 }
