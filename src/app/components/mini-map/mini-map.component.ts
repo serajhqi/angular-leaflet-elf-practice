c57f964d31ff5d22dd1565b133cd8d66b3c376b9
Seraj Haqiqi <haqiqi.seraj@gmail.com>
2022-09-26 21:56:01 +0330

    chore: Cleanup

--- src/app/components/mini-map/mini-map.component.ts
+++ src/app/components/mini-map/mini-map.component.ts
@@ -13,20 +13,16 @@ L.Marker.prototype.options.icon = L.icon({
 export class MiniMapComponent implements AfterViewInit {
 
   map!: L.Map;
-  @Input() location: L.LatLngExpression = [ 11.000512279195748,122.58819580078126 ];
+  @Input() location!: L.LatLngExpression;
   loading:boolean = false;
 
   constructor() {}
 
   ngAfterViewInit(): void {
     setTimeout(()=>{
-      this.map = L.map('mini-map')?.setView(this.location, 15);
-      const tile_layer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(this.map);
-
+      this.map = L.map('mini-map').setView(this.location, 15);
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(this.map);
       L.marker(this.location).addTo(this.map);
-
-      tile_layer.on("load",() =>  this.loading = false);
-      tile_layer.on("loading",() => this.loading = true );
     }, 1)
   }
 
