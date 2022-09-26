690cd6c231c99fee2094b0c8e19479a5b2445245
Seraj Haqiqi <haqiqi.seraj@gmail.com>
2022-09-26 12:13:33 +0330

    Squashed commit of the following:
    commit 34f72e082e7f172ca7a9fd3f691cb36e867f626c
    Author: Seraj Haqiqi <haqiqi.seraj@gmail.com>
    Date:   Mon Sep 26 10:09:47 2022 +0330
    
        fix: Fix footer style
    
    commit b9f589366261c34923a70ff8f490d1f848a50030
    Author: Seraj Haqiqi <haqiqi.seraj@gmail.com>
    Date:   Mon Sep 26 10:09:14 2022 +0330
    
        feat: Close popup on click on outside body
    
    commit 5d810855742a1a81ff0673485663ab699c83d869
    Author: Seraj Haqiqi <haqiqi.seraj@gmail.com>
    Date:   Mon Sep 26 10:07:46 2022 +0330
    
        feat: Add on close event
    
    commit fa02bc8860eb6bbf232e2581da37ab44e3821d40
    Author: Seraj Haqiqi <haqiqi.seraj@gmail.com>
    Date:   Mon Sep 26 09:41:50 2022 +0330
    
        feat: build popup essential structure
    
    commit a3f536c6bc8d77bfe9ed9c4ba9b8becf52dc3bff
    Author: Seraj Haqiqi <haqiqi.seraj@gmail.com>
    Date:   Mon Sep 26 08:25:45 2022 +0330
    
        init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapComponent } from './components/map/map.component';
+import { PopupComponent } from './components/popup/popup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent
+    MapComponent,
+    PopupComponent
   ],
   imports: [
     BrowserModule,
