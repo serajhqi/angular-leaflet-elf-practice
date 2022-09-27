a94fb43804318ea86449413dafc4d04916c94e6b
Seraj Haqiqi <haqiqi.seraj@gmail.com>
2022-09-27 03:54:42 +0330

    taskbar init

---
+++ src/app/taskbar/taskbar.component.ts
@@ -0,0 +1,38 @@
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { Location } from '../app.component';
+import { StorageService } from '../services/storage.service';
+
+@Component({
+  selector: 'app-taskbar',
+  templateUrl: './taskbar.component.html',
+  styleUrls: ['./taskbar.component.css']
+})
+export class TaskbarComponent implements OnInit,OnChanges {
+  @Input() title:string = '';
+  @Input() targetId?:string = '';
+  @Output() onEditClick: EventEmitter<string> = new EventEmitter();
+
+  constructor(private storageService: StorageService) { }
+
+  ngOnInit(): void {
+    this.findLocation()
+  }
+
+  location?: Location;
+  ngOnChanges(changes: SimpleChanges): void {
+    this.findLocation()
+  }
+
+  findLocation(){
+     this.location = this.targetId? this.storageService.getLocation(this.targetId)
+    : undefined;
+  }
+
+  onEditBnClick(){
+    this.onEditClick.emit(this.targetId);
+  }
+
+  onClearClick(){
+    this.storageService.clear()
+  }
+}
